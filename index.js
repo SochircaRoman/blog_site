a2eea728516ff68610aa61501709037ece591003
User01 <romansochirca30@gmail.com>
2022-09-06 11:25:30 +0300

    add database connection

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Post from './Post.js'
 
 const app = express()
 const port = 3000
+const DB_URL = 'mongodb+srv://user:user@cluster0.p4oeedo.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(express.json())
 
-app.post('/', (request, response) => {
-  console.log(request.body);
-  response.status(200).json('Server runing...')
+app.post('/', async (request, response) => {
+  try {
+    const {author, title, content, picture} = request.body
+    const post = await Post.create({author, title, content, picture})
+    response.json(post) 
+  } catch (error) {
+    response.status(500).json(error)
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const startApp = async () => {
+  try {
+    await mongoose.connect(DB_URL)
+    app.listen(port, () => {
+      console.log(`Server started on port http://localhost:${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+startApp()
+
